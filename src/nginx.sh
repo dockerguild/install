42aa560b40420ddac3e07b6d1f69b0e594aa0937
Jonathan SAHM <pro@jsahm.fr>
2024-02-02 11:53:48 +0100

    add dhparam

--- src/nginx.sh
+++ src/nginx.sh
@@ -2,9 +2,10 @@
 
 dg_nginx_install () {
   if [ ! -x /usr/sbin/nginx ]; then
-    sudo apt-get install -y nginx-extras
+    sudo apt-get install -y nginx-extras openssl
     sudo cp -vfr "${DGBASEPATH}/.install/nginx/"* /
     sudo mkdir -p /etc/nginx/sites-enabled/
+    sudo openssl dhparam -out /etc/nginx/certs/dhparams.pem 4096
     sudo service nginx restart
   fi
   /usr/sbin/nginx -V
